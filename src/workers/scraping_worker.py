082c156d43003cef26443e6d331dde8928eac906
Juan Godfrid <juan.godfrid@gmail.com>
2019-07-19 21:23:51 -0300

    Refactor of project directory structure.

---
+++ src/workers/scraping_worker.py
@@ -0,0 +1,12 @@
+import os
+
+import redis
+from rq import Worker
+from redis import Redis
+
+def work():
+    redis_conn = Redis()
+
+    parser = Worker(['scraping'], connection=redis_conn, name='scraper')
+
+    parser.work()
